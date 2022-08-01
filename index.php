0013057a968d865dd1c5716622f7dc04319ea88b
josh-lew <joshlewisdev@gmail.com>
2022-08-01 17:01:35 +0100

    updated with some new CSS which is a mess

--- index.php
+++ index.php
@@ -5,9 +5,27 @@ require_once "functions.php";
 $db = connectDB();
 $books = fetchAllDB($db);
 
-echo displayBooks($books);
+// echo displayBooks($books);
 
 
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+<title>Guidebooks</title>
+<link href="style.css" type="text/css" rel="stylesheet" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <section>
+        <h1>Josh's Guidebook Collection</h1>
+    </section>
+  <div class="bookGrid">
+        <?=  displayBooks($books); ?>
+  </div>
+</body>
+
+</html>
