3d9a9e27826bb7a7e842e18f7b28ed3638a7216a
josh-lew <joshlewisdev@gmail.com>
2022-08-01 14:51:05 +0100

    updated following code review

--- Testing/TestFunctions.php
+++ Testing/TestFunctions.php
@@ -1,20 +1,5 @@
 <?php
 
-
-// function displayBooks(array $books) {
-//     $result = '';
-//     foreach ($books as $book) {
-        
-//         $result .= '<p>Name: ' . $book['bookName'] . '</p>';
-//         $result .= '<p>Country: ' . $book['country'] . '</p>';
-//         $result .= '<p>Book Rating: ' . $book['bookRating'] . '</p>';
-//         $result .= '<p>Destination Rating: ' . $book['destinationRating'] . '</p>';
-//         $result .= '<p>Published: ' . $book['year'] . '</p><br><br>';
-//     }
-//     return $result;
-// }
-
-
 require_once '../functions.php';
 
 use PHPUnit\Framework\TestCase;
